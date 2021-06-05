69ae70d5bbf1dd49d3367ac20edcfacc2b9369f5
Your Name <you@example.com>
2021-06-05 22:34:42 +0300

    m

--- main.py
+++ main.py
@@ -50,38 +50,10 @@ def write_message():
         messages = [{'sender': message.sender.id, 'reciever': message.reciever.id, 'message': message.message, 'subject': message.subject, 'creation_date': str(message.creation_date),  'read': message.read} for message in messages]
         return make_response(jsonify(messages))
 
-@app.route("/messages/<message_id>/", methods=["GET", "DELETE"])
-@auth.login_required
-def write_message():
-    # import ipdb; ipdb.set_trace()
-    if request.method == "POST":
-        sender = auth.current_user()
-        reciever = request.json['reciever']
-        message = request.json['message']
-        subject = request.json['subject']
-        
-        message_object = Message(sender=sender, reciever=reciever,
-                             message=message, subject=subject)
-
-        return f"Message sent! sender is {sender.username}"
-
-    elif request.method == "GET":
-        import os
-        print("-----")
-        print(os.environ)
-        user = auth.current_user()
-        which_messages = request.values.get('which', 'all')
-        if which_messages == 'all': 
-            messages = select(m for m in Message if m.sender.id == user.id or m.reciever.id == user.id) # filter on this variable
-
-        else:
-            messages = select(m for m in Message if (m.sender.id == user.id or m.reciever.id == user.id) and (m.read == (which_messages == 'read')) )
-        messages = [{'sender': message.sender.id, 'reciever': message.reciever.id, 'message': message.message, 'subject': message.subject, 'creation_date': str(message.creation_date),  'read': message.read} for message in messages]
-        return make_response(jsonify(messages))
 
 @app.route("/messages/<message_id>/", methods=["GET", "DELETE"])
 @auth.login_required
-def message(message_id): # make sure user is allowed
+def message(message_id):
     message = Message[message_id]
     user = auth.current_user()
     if not (user == message.sender or user == message.reciever):
